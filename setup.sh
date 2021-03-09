cb3e129ba8093f57c1aff97edb4c9282f1b9c8b5
Joseph Welling <jow@alteos.com>
2021-03-09 15:08:12 +0100

    update

--- setup.sh
+++ setup.sh
@@ -22,6 +22,7 @@ open -a iTerm .
 echo "select the iterm2profile as your profile"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
 echo "Press Enter to continue"
 read -t 50000
 sh .macos
